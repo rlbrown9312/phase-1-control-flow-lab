4a32532328550dbcc5420c32ad53ff19321ec404
Richard <richard.brown0807@gmail.com>
2022-08-08 12:47:50 -0500

    commit tree 61ec06b715b4a4d6c523310be306cecd5c3e5351

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideFeet){
+    if (rideFeet <= 400) {
+      return "This one is on me!";
+    } else if (rideFeet >= 400 && rideFeet <= 2000) {
+        return "That will be twenty bucks.";
+    } else if (rideFeet > 2000 && rideFeet <= 2500) {
+        return "I will gladly take your thirty bucks.";
+    } else if (rideFeet > 2500) {
+        return "No can do.";
+    } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+    console.log(scuberGreetingForFeet(2001));
+
+
+function ternaryCheckCity(place) {
+  const city = "NYC"
+    return (place === "NYC"? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+//console.log(ternaryCheckCity("NYC")) 
+
+
+
+function switchOnCharmFromTip(tipType) {
+  switch (tipType) {
+      case "generous":
+        return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+      default:
+        return "Bye."
+  }
 }
+
+//console.log(switchOnCharmFromTip(6));
